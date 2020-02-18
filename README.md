f7c4195bb29841db4ae3f1d955e02dd59524085d
t.ochiai <t.ochiai.dev@gmail.com>
2020-02-18 09:31:35 +0900

    add README追記

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # app.proxy.local
 プロクシ用docker
+
+
+## 参考
+[jwilder/nginx-proxy](https://qiita.com/Ayumu_Usu/items/e01c5d4db693f09c5e53)
+[ブラウザにオレオレ証明書を登録する方法](https://kekaku.addisteria.com/wp/20190327053337)
--- app_clone.sh
+++ app_clone.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git clone https://github.com/yukichi0707/app.account ../app.account
-../app.account/setup.sh
+#../app.account/setup.sh
 git clone https://github.com/yukichi0707/app.bbs ../app.bbs
 # . ../app.bbs/setup.sh
 
