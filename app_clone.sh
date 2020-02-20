588d737dd2203cbc282c5586cafa0111ed17bacb
t.ochiai <t.ochiai.dev@gmail.com>
2020-02-20 16:34:31 +0900

    add

--- app_clone.sh
+++ app_clone.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-git clone https://github.com/yukichi0707/app.account ../app.account
-#../app.account/setup.sh
 git clone https://github.com/yukichi0707/app.bbs ../app.bbs
-# . ../app.bbs/setup.sh
+#../app.account/setup.sh
 
