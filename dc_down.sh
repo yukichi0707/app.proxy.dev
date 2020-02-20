87faa3075bbfb809aff40dc2195d8f7bfa320308
t.ochiai <t.ochiai.dev@gmail.com>
2020-02-21 01:37:20 +0900

    add network名修正

--- dc_down.sh
+++ dc_down.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker-compose down --rmi all
-docker network rm common_link
+docker network rm common_network
 
 
--- dc_up.sh
+++ dc_up.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker network create --driver bridge common_link
+docker network create --driver bridge common_network
 docker-compose up -d
 
